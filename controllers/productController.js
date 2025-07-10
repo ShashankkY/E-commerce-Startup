971e5dd11889e31cdc181bcc7938b19a2030dd54
ShashankkY <shashankyogesh12@gmail.com>
2025-07-10 11:56:19 +0530

    Minor update

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,10 +4,10 @@ exports.getAllProducts = (req, res) => {
   res.send("Fetching all products");
 };
 
-exports.getProductById = (req, res) => {
-  res.send(`Fetching product with ID: ${req.params.id}`);
-};
-
 exports.addProduct = (req, res) => {
   res.send("Adding a new product");
+};
+
+exports.getProductById = (req, res) => {
+  res.send(`Fetching product with ID: ${req.params.id}`);
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,10 @@ exports.getAllUsers = (req, res) => {
   res.send("Fetching all users");
 };
 
-exports.getUserById = (req, res) => {
-  res.send(`Fetching user with ID: ${req.params.id}`);
-};
-
 exports.addUser = (req, res) => {
   res.send("Adding a new user");
+};
+
+exports.getUserById = (req, res) => {
+  res.send(`Fetching user with ID: ${req.params.id}`);
 };
